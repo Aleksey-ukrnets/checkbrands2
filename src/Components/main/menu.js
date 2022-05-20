c00591bb3b130b86536b2208e61d8b526220f727
Aleksey-ukrnets <aleksey.ukrnets@mail.ru>
2022-05-20 16:40:55 +0300

    корректировки

--- src/Components/main/menu.js
+++ src/Components/main/menu.js
@@ -5,10 +5,9 @@ import './style.scss'
 const Menu = () => {
     return(
         <>
-        <div className='Link' style={{display:'flex'}}>
-            <NavLink to='/' className="Link-routes">Persons</NavLink>
-            <NavLink to='/brands' className="Link-routes">Brand</NavLink>
-            <NavLink to='/stats' className="Link-routes">Statisctic</NavLink>
+        <div className='Link' style={{display:'fle<NavLink to='/stats' className="Link-routes">stats</NavLink>
+            <NavLink to='/' className="Link-routes">persons</NavLink>
+            <NavLink to='/brands' className="Link-routes">brands</NavLink>
         </div>
         </>
     )
